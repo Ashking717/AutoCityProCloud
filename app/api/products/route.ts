18dda1887e82f608606672613518e88c36590095
muhammed ashiq <muhd.ashi@gmail.com>
2026-01-17 15:00:29 +0530

    minor3

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -36,7 +36,7 @@ export async function GET(request: NextRequest) {
     
     const query: any = {
       outletId: outletIdObj,
-      isActive: true,
+      
     };
     
     // ─────────────────────────────────────────────────────────────
